19b2f1e507e87182163a970db863dd78d052e83f
MohamedOsama <mo5709813@gmail.com>
2026-04-21 02:00:27 +0200

    start

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(3000, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+export default app;
