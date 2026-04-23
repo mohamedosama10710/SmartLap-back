9ecb1f59309436aee3f5c1c075e0e7c19767407f
Marwa Emam <marwaemam2411@gmail.com>
2026-04-23 15:38:50 +0200

    Add User and Report schemas with medical logic and trend

---
+++ Models/Report.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose')
+const reportSchema= new mongoose.Schema({
+
+
+patient:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true},
+
+    testName:{
+        type:String,
+        required:true
+    },
+    testCategory:{
+        type:String
+    },
+    status:{
+        type:String,
+        enum:['H','N','L',''],
+        default:''
+    },
+    date: { type: Date, default: Date.now },
+    referenceRange: { type: String }, 
+trend: {
+    direction: {
+         type: String, 
+        enum: ['improving', 'declining', 'stable', 'none'] },
+    note: String
+  }
+
+})
+let reportModel=mongoose.model('Report',reportSchema);
+module.exports=reportModel
