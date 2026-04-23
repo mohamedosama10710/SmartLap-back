d78adce61fc79f8b3e914256e0378dd71f372080
MohamedOsama <mo5709813@gmail.com>
2026-04-23 16:25:18 +0200

    marwa

--- src/config/dp.js
+++ src/config/dp.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import {testReference} from "../models/testReference.js"
-import {data} from "../utils/testsArray.js"
+import { data } from "../utils/testsArray.js"
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    // await testReference.insertMany(data) ;
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.error(error.message);
