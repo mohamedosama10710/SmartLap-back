306d080fbabe8ee2f854c4f370900ac2fd3a62ea
MohamedOsama <mo5709813@gmail.com>
2026-05-07 00:16:48 +0300

    middleware

--- src/routes/labSettingsRoutes.js
+++ src/routes/labSettingsRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { auth, restrictTo } from "../middlewares/authMiddleware.js";
+import { auth, restrictTo } from "../middlewares/middleware.js";
 import { getSchedule, updateSchedule } from "../Controllers/labSettings.js";
 
 const router = express.Router();
