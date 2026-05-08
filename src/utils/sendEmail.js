3c6d7680af21d6b6bcd575ef0bb809e2ea016269
MohamedOsama <mo5709813@gmail.com>
2026-05-08 23:45:49 +0300

    update

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,30 +1,29 @@
-import nodemailer from'nodemailer';
-let sendEmail= async(options)=>{
-try{
-    const tranporter=nodemailer.createTransport
-({
-    host:process.env.EMAIL_HOST,
-    port:process.env.EMAIL_PORT,
-    secure:false,
-    auth:{
-    user:process.env.EMAIL_USER,
-    pass:process.env.EMAIL_PASSWORD
-    },
-  tls: {
-    rejectUnauthorized: false 
-  }
-});
+import nodemailer from "nodemailer";
+let sendEmail = async (options) => {
+  try {
+    const tranporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
 
-const emailoptions={
-    from:'"Smart Lab " <smartlap458@gmail.com>',
-    to:options.email,
-    subject:options.subject,
-    html:options.html
-}
-await tranporter.sendMail(emailoptions);
-}
-catch(error){
+    const emailoptions = {
+      from: '"Smart Lab " <smartlap458@gmail.com>',
+      to: options.email,
+      subject: options.subject,
+      html: options.html,
+    };
+    await tranporter.sendMail(emailoptions);
+  } catch (error) {
+    console.error("Detailed Email Error:", error); // ضيف السطر ده ضروري
     throw error;
-}
+  }
 };
-export{sendEmail};
+export { sendEmail };
